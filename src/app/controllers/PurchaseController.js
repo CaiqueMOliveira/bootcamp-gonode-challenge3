a383e343265d976ae711a587d349c5807c4fc07a
Caique M. Oliveira <caique.m.oliveira.br@gmail.com>
2019-05-23 20:52:07 -0300

    store purchase in database and filter purchases that has already been purchased

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -1,5 +1,6 @@
 const Ad = require('../models/Ad')
 const User = require('../models/User')
+const Purchase = require('../models/Purchase')
 const PurchaseMail = require('../jobs/PurchaseMail')
 const Queue = require('../services/Queue')
 
@@ -8,11 +9,25 @@ class PurchaseController {
     const { adId, messageToSeller } = req.body
 
     const foundAd = await Ad.findById(adId).populate('author')
+
+    if (foundAd.purchasedBy) {
+      return res
+        .status(409)
+        .json({ error: 'This product has already been purchased' })
+    }
+
     const client = await User.findById(req.userId)
+
+    const newPurchseCreated = await Purchase.create({
+      client,
+      product: foundAd
+    })
+
+    if (!newPurchseCreated) throw new Error()
 
     Queue.create(PurchaseMail.key, { foundAd, client, messageToSeller }).save()
 
-    return res.send()
+    return res.send(newPurchseCreated)
   }
 }
 
